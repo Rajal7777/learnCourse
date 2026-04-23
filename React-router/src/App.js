58ea4019a3c4b8a2d1f9bd685b6b38ca9deb58a4
Rajal7777 <suwalrajal57@gmail.com>
2026-04-23 13:12:35 +0900

    routes

--- React-router/src/App.js
+++ React-router/src/App.js
@@ -1,5 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//alternate
+// import  { createRoutesFromElements, Route} from 'react-router-dom';
+
+
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
+
+
+//Alternate Method
+/*
+const routeDefinations = createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/product" element={<ProductPage />} />
+    </Route>
+)
+const router = createBrowserRouter(routeDefinations);
+
+*/
+
+
+
+//Create the router configuration
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: '/product', element: <ProductPage />}
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ React-router/src/pages/ProductPage.js
@@ -0,0 +1,4 @@
+
+export default function ProductPage(){
+    return <h2>Product page</h2>
+}
