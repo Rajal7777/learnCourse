928d6771e7cd668842672e221e4917909c0ef305
Rajal7777 <suwalrajal57@gmail.com>
2026-04-23 18:46:27 +0900

    relative path useing useParams

--- React-router/src/App.js
+++ React-router/src/App.js
@@ -1,24 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-//alternate
-// import  { createRoutesFromElements, Route} from 'react-router-dom';
+
 
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ProductDetailPage from "./pages/ProductDetail";
 
-//Alternate Method
-/*
-const routeDefinations = createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/product" element={<ProductPage />} />
-    </Route>
-)
-const router = createBrowserRouter(routeDefinations);
 
-*/
 
 //Create the router configuration
 //incase of error react-router provides a errorElement where we can display the Error handeling UI,
@@ -30,6 +20,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> },
     ],
   }
 ]);
